19a9833e12c87dbfbb5d16e9e3b80e83e88d92db
nithyamani <nithya.mn999@gmail.com>
2022-03-02 16:43:41 +0530

    added build name

--- android/examples/run-first-test/first.conf.js
+++ android/examples/run-first-test/first.conf.js
@@ -10,7 +10,7 @@ exports.config = {
 
   capabilities: [{
     project: "First Webdriverio Android Project",
-    build: 'Webdriverio Android',
+    build: process.env.BROWSERSTACK_BUILD_NAME,
     name: 'first_test',
     device: 'Google Pixel 3',
     os_version: "9.0",
